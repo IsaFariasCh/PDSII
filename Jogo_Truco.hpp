ceef003dd9c285b8c5d1f781b39993fde75c1290
iaraalvest <iarq765@gmail.com>
2024-06-20 22:38:16 -0300

    Update Jogo_Truco.hpp

--- Jogo_Truco.hpp
+++ Jogo_Truco.hpp
@@ -1,14 +1,26 @@
- #ifndef JOGO_TRUCO_HPP
- #define JOGO_TRUCO_HPP
+#ifndef JOGO_TRUCO_HPP
+#define JOGO_TRUCO_HPP
 
+#include <iostream>
+#include <vector>
+#include <string>
 #include "jogo.hpp"
 
-class Jogo_Truco : protected jogo
-{
-private:
-protected:
+class Jogo_Truco : public IJogo {
 public:
+    Jogo_Truco(); // Construtor para inicializar o jogo
+    void explica_jogo();
+    void Start();
 
-}
+private:
+    void distribuir_cartas();
+    void jogar_rodada();
+    void mostrar_cartas();
 
- #endif
+    std::vector<std::string> jogadores;
+    std::vector<std::string> baralho;
+    std::vector<std::string> cartas_jogador1;
+    std::vector<std::string> cartas_jogador2;
+};
+
+#endif
