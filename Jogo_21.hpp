91e3a1f04c3b100b13e37dc05ea239b6465835e2
iaraalvest <iarq765@gmail.com>
2024-06-21 21:37:27 -0300

    Update Jogo_21.hpp

--- Jogo_21.hpp
+++ Jogo_21.hpp
@@ -1,16 +1,40 @@
  #ifndef JOGO_21_HPP
  #define JOGO_21_HPP
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "jogo.hpp"
 
-class jogo;
 
-class  jogo :  Jogo_21
-{
+
+
+class Jogo_21  :  public IJogo {
+
 private:
+std::string nomes_jogadores[2];
+std::string Dar_carta();
+
 protected:
+
+std::string Get_Nome_Jogador(int n){return nomes_jogadores[n];};
+
 public:
-  void Start
+
+Jogo_21(){};
+
+Jogo_21(string nome1,string nome2)
+  {
+  nomes_jogadores[0]=nome1;
+  nomes_jogadores[1]=nome2;
+
+  };
+
+  void explica_jogo()override; 
+  void Start()override;
+  void declarar_vencedor(int,int);
+
 
 };
 
