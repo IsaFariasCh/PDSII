167ddb2a25c5dfeee65ba2205083443108815e2a
iaraalvest <iarq765@gmail.com>
2024-07-13 17:50:24 -0300

    Update jogador.hpp

--- jogador.hpp
+++ jogador.hpp
@@ -22,3 +22,38 @@ std::string nome;
 };
 
   #endif
+
+
+
+
+// Atualizações após implementação da programação defensiva 
+#ifndef JOGADOR_HPP
+#define JOGADOR_HPP
+
+#include <iostream>
+#include <string>
+#include "casa.hpp"
+
+class casa;
+
+class jogador
+{
+    friend casa;
+
+private:
+    std::string nome;
+
+public:
+    std::string Get_nome();
+    jogador(std::string entrada_nome)
+    {
+        if (entrada_nome.empty())
+        {
+            std::cerr << "Nome do jogador não pode ser vazio. Ajustando para 'Jogador'" << std::endl;
+            entrada_nome = "Jogador";
+        }
+        this->nome = entrada_nome;
+    }
+};
+
+#endif
