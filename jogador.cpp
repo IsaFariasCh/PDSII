07f91d6ad4b2800956b627fa27321164f82ab72f
iaraalvest <iarq765@gmail.com>
2024-07-13 17:57:17 -0300

    Update jogador.cpp

--- jogador.cpp
+++ jogador.cpp
@@ -10,3 +10,21 @@ std::string jogador:: Get_nome(){return this->nome;};
     {
         this->nome=entrada_nome;
     };
+
+
+// Atualizações após implementação da programação defensiva
+#include <iostream>
+#include "jogador.hpp"
+
+// respectivas funções para o atributo nome
+std::string jogador::Get_nome() { return this->nome; }
+
+jogador::jogador(std::string entrada_nome)
+{
+    if (entrada_nome.empty())
+    {
+        std::cerr << "Nome do jogador não pode ser vazio. Ajustando para 'Jogador'" << std::endl;
+        entrada_nome = "Jogador";
+    }
+    this->nome = entrada_nome;
+}
