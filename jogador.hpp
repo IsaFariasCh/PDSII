741b60c05f674d3ade9c3e8b89efd8ab7e9f9aa2
iaraalvest <iarq765@gmail.com>
2024-07-13 17:59:51 -0300

    Update jogador.hpp

--- jogador.hpp
+++ jogador.hpp
@@ -57,3 +57,29 @@ public:
 };
 
 #endif
+
+
+
+// Atualizações após a implementação da programação defensiva 
+#ifndef JOGADOR_HPP
+#define JOGADOR_HPP
+
+#include <iostream>
+#include <string>
+#include "casa.hpp"
+
+class casa;
+
+class jogador
+{
+    friend casa;
+
+private:
+    std::string nome;
+
+public:
+    std::string Get_nome();
+    jogador(std::string entrada_nome);
+};
+
+#endif
